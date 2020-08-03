db04f552e8e727861961c436b2ab46ef0741e80b
haywhizzz <haywhizzz@gmail.com>
2020-08-03 11:14:52 +0100

    Add if condition for bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,13 @@
 def bubble_sort(arr)
   i = 0
   while i < arr.length - 1
+    if arr[i] > arr[i + 1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        i = 0
+      else
+        i += 1
+      end
+      p arr
   end
+  p arr
 end
