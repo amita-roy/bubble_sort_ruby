38c50c9781de2e45e09355f4ec2d615511314e94
Amita <sardana.amita2@gmail.com>
2020-08-03 15:49:19 +0530

    add bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,12 +2,23 @@ def bubble_sort(arr)
   i = 0
   while i < arr.length - 1
     if arr[i] > arr[i + 1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
-        i = 0
-      else
-        i += 1
-      end
-      p arr
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      i = 0
+    else
+      i += 1
+    end
+    p arr
   end
   p arr
+end
+
+bubble_sort([4, 9, 5, 3, 1])
+
+def bubble_sort_by(arr)
+  i = 0
+  while i < arr.length - 1
+    if yield(arr[i], arr[i + 1]).positive?
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    
+  end
 end
